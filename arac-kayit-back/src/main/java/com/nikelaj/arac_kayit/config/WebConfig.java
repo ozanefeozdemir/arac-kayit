69bbccedc290de4d276faf9cb9e2ced5ca8f0d6f
ozanefeozdemir <ozanefeozdemir@hotmail.com>
2026-07-12 13:10:28 +0300

    .

--- arac-kayit-back/src/main/java/com/nikelaj/arac_kayit/config/WebConfig.java
+++ arac-kayit-back/src/main/java/com/nikelaj/arac_kayit/config/WebConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173"})
+@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173", "https://arac-kayit-7ub7ew2db-ozanefeozdemirs-projects.vercel.app/"})
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
