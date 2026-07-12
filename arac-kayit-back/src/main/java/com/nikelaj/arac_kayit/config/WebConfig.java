e59f14896080adc8691e53319dbb6ad3c0145f48
ozanefeozdemir <ozanefeozdemir@hotmail.com>
2026-07-12 13:25:40 +0300

    .

--- arac-kayit-back/src/main/java/com/nikelaj/arac_kayit/config/WebConfig.java
+++ arac-kayit-back/src/main/java/com/nikelaj/arac_kayit/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173", "https://arac-kayit-7ub7ew2db-ozanefeozdemirs-projects.vercel.app/")
+                .allowedOrigins("http://localhost:5173", "https://arac-kayit-7ub7ew2db-ozanefeozdemirs-projects.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
